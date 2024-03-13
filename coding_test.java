eff93350d1d9bb5e59538f58ec456edc9331c3ae
nahollo <skgudwlsdlrj@gmail.com>
2024-03-13 14:40:31 +0900

    LV2_주식가격

--- coding_test.java
+++ coding_test.java
@@ -1,17 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
-	public int solution(int[] A, int[] B) {
-		int answer = 0;
-		int j = B.length;
-
-		Arrays.sort(A);
-		Arrays.sort(B);
-
-		for (int i = 0; i < A.length; i++) {
-			answer += A[i] * B[j - 1];
-			j--;
-		}
-		return answer;
-	}
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length]; // 리턴값
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] > prices[j]) { // 앞에 가격이 더 클 때
+                    answer[i]++; // 앞에 값이 더 클 때 == 가격이 떨어졌을 때
+                    break; // 가격이 떨어지면 값 비교하는 부분 break
+                } else {
+                    answer[i]++; // 가격이 안떨어졌을 때는 answer값 1씩 증가
+                }
+            }
+        }
+        return answer;
+    }
 }
