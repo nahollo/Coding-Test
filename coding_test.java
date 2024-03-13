33ca663eda3d9799bc18a28428f985192bad1542
nahollo <skgudwlsdlrj@gmail.com>
2024-03-13 14:41:26 +0900

    LV3_정수 삼각형

--- coding_test.java
+++ coding_test.java
@@ -1,17 +1,10 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int yellow_Width;
-        for (int yellow_Length = 1; yellow_Length <= yellow / 2 + 1; yellow_Length++) {
-            if (yellow % yellow_Length == 0) {
-                yellow_Width = yellow / yellow_Length;
-                if ((2 * yellow_Length + 2 * yellow_Width + 4 == brown) && (yellow_Length >= yellow_Width)) {
-                    answer[0] = yellow_Length + 2;
-                    answer[1] = yellow_Width + 2;
-                    return answer;
-                }
-            }
-        }
-        return answer;
-    }
+	public int solution(int[][] triangle) {
+		for (int i = triangle.length - 1; i > 0; i--) {
+			for (int j = 0; j < triangle[i].length - 1; j++) {
+				triangle[i - 1][j] += Math.max(triangle[i][j], triangle[i][j + 1]); 
+			}
+		}
+		return triangle[0][0];
+	}
 }
