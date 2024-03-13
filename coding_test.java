473e56584717fa040ac7751d2418fc319322a08f
nahollo <skgudwlsdlrj@gmail.com>
2024-03-13 14:39:19 +0900

    LV2_멀리 뛰기

--- coding_test.java
+++ coding_test.java
@@ -1,23 +1,11 @@
 class Solution {
-	boolean solution(String s) {
-		boolean answer = true;
-		int count = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '(') {
-				count++;
-			} else {
-				if(count == 0) {
-					return false;
-				}
-				count--;
-			}
-		}
-
-		if (count == 0 && (s.charAt(0) == '(' && s.charAt(s.length() - 1) == ')')){
-			
-		} else {
-			answer = false;
-		}
-		return answer;
-	}
+    public long solution(int n) {
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < 2001; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        }
+        return dp[n];
+    }
 }
