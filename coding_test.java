beabcb13fb309736febccd77d6df8793e19e5c43
nahollo <skgudwlsdlrj@gmail.com>
2024-03-13 14:41:59 +0900

    LV3_단어 변환

--- coding_test.java
+++ coding_test.java
@@ -1,10 +1,57 @@
 class Solution {
-	public int solution(int[][] triangle) {
-		for (int i = triangle.length - 1; i > 0; i--) {
-			for (int j = 0; j < triangle[i].length - 1; j++) {
-				triangle[i - 1][j] += Math.max(triangle[i][j], triangle[i][j + 1]); 
+	String begin;
+	String target;
+	String[] words;
+	int[] visit;
+	int min;
+
+	public int solution(String begin, String target, String[] words) {
+		this.begin = begin;
+		this.target = target;
+		this.words = words;
+		visit = new int[words.length];
+		int difcount = 0;
+
+		min = words.length;
+
+		for (int i = 0; i < words.length; i++) {
+			if (target.equals(words[i])) {
+				difcount++;
 			}
 		}
-		return triangle[0][0];
+		if (difcount == 0) {
+			return 0;
+		}
+		
+		dfs(begin, 0);
+
+		return min;
+	}
+
+	void dfs(String word, int step) {
+		if (word.equals(target)) {
+			min = Math.min(min, step);
+			return;
+		}
+		for (int i = 0; i < words.length; i++) {
+			if (visit[i] == 0 && diffrent_Check(word, words[i])) {
+				visit[i] = 1;
+				dfs(words[i], step + 1);
+				visit[i] = 0;
+			}
+		}
+	}
+
+	boolean diffrent_Check(String word1, String word2) { // 한개만 다른지 체크하는 메소드
+		int diffrent_Count = 0; // 두 String의 다른만큼 카운트 해주는 변수
+		for (int i = 0; i < word1.length(); i++) {
+			if (word1.charAt(i) != word2.charAt(i)) {
+				diffrent_Count++;
+				if (diffrent_Count > 1) { // 한개 이상으로 다를 때
+					return false; // 할 수 없다
+				}
+			}
+		}
+		return diffrent_Count == 1;
 	}
 }
