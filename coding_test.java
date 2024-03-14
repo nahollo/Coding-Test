a0f400572200fc52793bf132ab2893014850d643
nahollo <skgudwlsdlrj@gmail.com>
2024-03-14 09:47:33 +0900

    LV1_직사각형 별찍기

--- coding_test.java
+++ coding_test.java
@@ -1,41 +1,16 @@
+import java.util.Scanner;
+
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        char[] num = new char[s.length()]; // s를 char 배열로 받기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        for (int i = 0; i < s.length(); i++) {
-            num[i] = s.charAt(i);
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        if (num[0] == '-') { // s의 맨 앞이 -일 때
-            for (int i = 1; i < s.length(); i++) {
-                answer = answer + (num[s.length() - i] - '0') * j(i - 1);
-            }
-            answer = answer * (-1);
-            System.out.println(answer);
-        } else if (num[0] == '+') { // s의 맨 앞이 +일 때
-            for (int i = 1; i < s.length(); i++) {
-                answer = answer + (num[s.length() - i] - '0') * j(i - 1);
-            }
-            System.out.println(answer);
-        } else {
-            for (int i = 0; i < s.length(); i++) { // s의 맨 앞에 아무것도 없을 때
-                answer = answer + (num[s.length() - i - 1] - '0') * j(i);
-            }
-        }
-
-        return answer;
-    }
-
-    public int j(int x) { // 10의 제곱을 만드는 메서드
-        int temp = 1;
-        if (x == 0) {
-            return 1;
-        } else {
-            for (int i = 0; i < x; i++) {
-                temp = temp * 10;
-            }
-            return temp;
-        }
-
     }
 }
