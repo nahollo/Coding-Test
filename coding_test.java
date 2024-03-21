e99e3ff50061f02e2fed4b96ad1e08fedc6cd336
nahollo <skgudwlsdlrj@gmail.com>
2024-03-21 13:51:19 +0900

    LV1_서울에서 김서방 찾기

--- coding_test.java
+++ coding_test.java
@@ -1,24 +1,11 @@
 class Solution {
-    public String solution(String s) {
+    public String solution(String[] seoul) {
         String answer = "";
-        char[] temp = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            temp[i] = s.charAt(i);
-        }
-        int cnt = 0;
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i] == ' ') {
-                answer += " ";
-                cnt = 0;
-            } else if (cnt % 2 == 0) {
-                answer += Character.toUpperCase(temp[i]);
-                cnt++;
-            } else {
-                answer += Character.toLowerCase(temp[i]);
-                cnt++;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                return "김서방은 " + i + "에 있다";
             }
         }
-
         return answer;
     }
 }
