6b087e75ce7daa1e16f477913e5aca360b840d07
nahollo <skgudwlsdlrj@gmail.com>
2024-04-23 17:37:15 +0900

    LV2_숫자의 표현

--- Solution.java
+++ Solution.java
@@ -1,9 +1,19 @@
-public class Solution {
+class Solution {
     public int solution(int n) {
-        String num = String.valueOf(n);
+        // 최솟값을 1 씩 더해가며 연속된 자연수를 더해 n 나오는지 체크하기
         int answer = 0;
-        for (int i = 0; i < num.length(); i++) {
-            answer += (num.charAt(i)-'0');
+        int sum = 0;
+        for (int i = 1; i <= n; i++) { // 더하고자 하는 최솟값을 1부터 1씩 증가시키기
+            sum = 0;
+            for (int j = i; j <= n; j++) { // 최솟값부터 1씩 증가시키며 연속된 자연수를 체크하기
+                sum += j;
+                if (sum == n) { // 모두 더한 값과 n이 같으면 표현 가능한 방법 1 증가
+                    answer++;
+                    break;
+                } else if (sum > n) { // 덧셈으로 n을 넘어가면 덧셈 종료
+                    break;
+                }
+            }
         }
         return answer;
     }
